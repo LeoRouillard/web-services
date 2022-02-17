1027e16ef2c880b547e2396b817605b1ad1164a1
davik13 <aime.bea@hotamail.com>
2022-02-17 15:07:19 +0100

    vue install

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
@@ -15,7 +17,7 @@
   color: #2c3e50;
 }
 
-#nav {
+nav {
   padding: 30px;
 
   a {
