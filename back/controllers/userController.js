4e3dc04b0671c43c6d299a5008f75c1aa69749e8
davik13 <aime.bea@hotamail.com>
2022-03-02 19:52:35 +0100

    crud user

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 
+
+//GET ALL USER
 exports.FindAll = async (req, res) => {
   const query = req.query.new;
   try {
@@ -12,6 +14,7 @@ exports.FindAll = async (req, res) => {
   }
 };
 
+//GET USER
 exports.FindById = async (req, res) => {
   try {
       const user = await User.findById(req.params.id);
@@ -22,6 +25,7 @@ exports.FindById = async (req, res) => {
   }
 };
 
+//DELETE
 exports.Delete = async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id);
@@ -31,13 +35,50 @@ exports.Delete = async (req, res) => {
     }
 }
 
+//CREATE
 exports.Create = async (req, res) => {
     try {
         if(!req.body){
             return res.status(400).send({message: "All fields are required"})
         }
-        
+        const user = new User({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          phone: req.body.phone,
+
+        });
+
+      await user.save().then(data => {
+        res.send(data);
+      })
     } catch (error) {
-        
+        res.status(500).send({message: error.message || 'user not create '});
     }
+}
+
+//UPDATE
+exports.Update = async (req, res) => {
+  try {
+    if(!req.body){
+      return res.status(400).send({message: 'All fields are required'}) 
+    }
+    
+    const updateUser = await User.findbyIdAndUpdate(req.params.id,
+      {
+        $set: req.body
+      },
+      {new: true}).then(user => {
+        if(!user){
+          return res.status(404).send({message: 'user not found' + req.params.id})
+        }
+        
+      });
+      res.status(200).json(updateUser);
+      
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
--- back/models/userModel.js
+++ back/models/userModel.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
     {
+        firstName:{type: 'string',required: true},
+        lastName:{type: 'string',required: true},
         username:{type: 'string',required: true, unique: true},
         email:{type: 'string',required: true, unique: true},
         password:{type: 'string',required: true},
