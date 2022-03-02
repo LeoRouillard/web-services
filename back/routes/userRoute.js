9bf3689fff1360fd75ee3a34e59f8c4d4326fa7f
davik13 <aime.bea@hotamail.com>
2022-03-02 19:52:12 +0100

    crud user

--- back/routes/userRoute.js
+++ back/routes/userRoute.js
@@ -4,6 +4,14 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
+//route Public
 router.get('/', userController.FindAll)
+router.get('/:id', userController.FindById);
+router.post('/', userController.Create);
+
+
+//route Private
+router.put('/:id', userController.Update);
+router.delete('/:id', userController.Delete);
 
 module.exports = router;
